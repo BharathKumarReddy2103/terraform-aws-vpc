31464782127d494208aee34cb0bdc14eb0a7b471
BharathKumarReddy <nbkumar2103@gmail.com>
2026-02-09 13:23:50 +0530

    updated file

--- peering.tf
+++ peering.tf
@@ -1,16 +1,16 @@
-# resource "aws_vpc_peering_connection" "default" {
-#   count = var.is_peering_required ? 1 : 0
+resource "aws_vpc_peering_connection" "default" {
+  count = var.is_peering_required ? 1 : 0
 
-#   peer_vpc_id   = data.aws_vpc.default.id # acceptor VPC which is default here
-#   vpc_id        = aws_vpc.main.id # requestor
+  peer_vpc_id   = data.aws_vpc.default.id # acceptor VPC which is default here
+  vpc_id        = aws_vpc.main.id # requestor
 
-#   accepter {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   requester {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 
 #   auto_accept = true
 
@@ -50,4 +50,4 @@
 #   route_table_id            = data.aws_route_table.main.id
 #   destination_cidr_block    = var.cidr_block
 #   vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
-# }
+}
