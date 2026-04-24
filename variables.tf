0bd55f17368117641a710c25602c71cd0d411cf8
BharathKumarReddy <nbkumar2103@gmail.com>
2026-04-24 20:07:44 +0530

    updated file

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "cidr_block" {
     default = "10.0.0.0/16"
 }
 
-variable "vpc_tags" {
-    type = map(string)
-    default = {}
-}
+# variable "vpc_tags" {
+#     type = map(string)
+#     default = {}
+# }
 
 # variable "igw_tags"{
 #     type = map(string)
