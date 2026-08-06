ecf51b64c51c94dd342b4cf52411425a91fb4965
BharathKumarReddy <nbkumar2103@gmail.com>
2026-08-07 05:17:55 +0530

    updated file

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "cidr_block" {
     default = "10.0.0.0/16"
 }
 
-# variable "vpc_tags" {
-#     type = map(string)
-#     default = {}
-# }
+variable "vpc_tags" {
+    type = map(string)
+    default = {}
+}
 
 variable "igw_tags"{
     type = map(string)
@@ -29,24 +29,24 @@ variable "public_subnet_tags" {
     default = {}
 }
 
-# variable "private_subnet_cidrs" {
-#     type = list(string)
-# }
+variable "private_subnet_cidrs" {
+    type = list(string)
+}
 
 
-# variable "private_subnet_tags" {
-#     type = map(string)
-#     default = {}
-# }
+variable "private_subnet_tags" {
+    type = map(string)
+    default = {}
+}
 
-# variable "database_subnet_cidrs" {
-#     type = list(string)
-# }
+variable "database_subnet_cidrs" {
+    type = list(string)
+}
 
-# variable "database_subnet_tags" {
-#     type = map(string)
-#     default = {}
-# }
+variable "database_subnet_tags" {
+    type = map(string)
+    default = {}
+}
 
 # variable "eip_tags" {
 #     type = map(string)
--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = "true"
 
   tags = merge(
-    # var.vpc_tags,
+    var.vpc_tags,
     local.common_tags,
     {
         Name = "${var.project}-${var.environment}"
@@ -44,37 +44,37 @@ resource "aws_subnet" "public" {
   )
 }
 
-# resource "aws_subnet" "private" {
-#   count = length(var.private_subnet_cidrs)
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = var.private_subnet_cidrs[count.index]
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
   
-#   availability_zone = local.az_names[count.index]
+  availability_zone = local.az_names[count.index]
 
-#   tags = merge(
-#     var.private_subnet_tags,
-#     local.common_tags,
-#     {
-#         Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
-#     }
-#   )
-# }
+  tags = merge(
+    var.private_subnet_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+    }
+  )
+}
 
-# resource "aws_subnet" "database" {
-#   count = length(var.database_subnet_cidrs)
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = var.database_subnet_cidrs[count.index]
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
   
-#   availability_zone = local.az_names[count.index]
+  availability_zone = local.az_names[count.index]
 
-#   tags = merge(
-#     var.database_subnet_tags,
-#     local.common_tags,
-#     {
-#         Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
-#     }
-#   )
-# }
+  tags = merge(
+    var.database_subnet_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+    }
+  )
+}
 
 # resource "aws_eip" "nat" {
 #   domain   = "vpc"
