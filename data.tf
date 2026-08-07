f049a05320bbc3ed78e09e1b64045bca9d03ade9
BharathKumarReddy <nbkumar2103@gmail.com>
2026-08-07 06:15:14 +0530

    updated file

--- data.tf
+++ data.tf
@@ -2,9 +2,9 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# data "aws_vpc" "default" {
-#   default = true
-# }
+data "aws_vpc" "default" {
+  default = true
+}
 
 # output "azs_info" {
 #     value = data.aws_availability_zones.available
